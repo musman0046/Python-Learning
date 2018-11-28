f0f063e5722ff6d799addf9399f0c6e40ed58165
usmancielo <usman@cielowigle.com>
2018-11-28 16:54:30 +0500

    add python decorater basic code

--- decorator.py
+++ decorator.py
@@ -11,18 +11,15 @@ __desc__        = Basic Implementation of python decorater
 def decorater(fun):
     #that function who will decorate a,b are the parameters that will come from the called function e.g fun
     def wrap(a,b):
-
         print("some thing happens before function decorate")
         if b == 0:
             return False
         return fun(a, b)
     return wrap
 
-
 @decorater
 def add(a,b):
     return "divide result is" , int(a/b)
-
 
 res=add(2,2)
 print(res)
